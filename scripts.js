ee764a4c4dd90f088ed4f6c85e38a83b1b2f1de4
arkadiusoo <arkadiusz.mlyn@icloud.com>
2024-10-22 23:40:23 +0200

    step 3a - done

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
 "use strict"
 let todoList = []; //declares a new array for Your todo list
 
+// step 3.a - test data
+let initList = function() {
+    todoList.push(
+    {
+        title: "Learn JS",
+        description: "Create a demo application for my TODO's",
+        place: "445",
+        category: '',
+        dueDate: new Date(2024,10,16)
+    },
+    {
+        title: "Lecture test",
+        description: "Quick test from the first three lectures",
+        place: "F6",
+        category: '',
+        dueDate: new Date(2024,10,17)
+    }
+        // of course the lecture test mentioned above will not take place
+    );
+}
+
+initList();
